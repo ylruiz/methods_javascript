aa6e88f9e6cf7f7af8a57f67db9bce7888c13506
Yunet Luis Ruiz <yunet.lruiz94@gmail.com>
2019-05-08 13:19:30 -0300

    add boolean-variable.html and boolean-variable.css

---
+++ c02/js/boolean-variable.js
@@ -0,0 +1,8 @@
+const inStock =  true;
+const shipping = false;
+
+const elStock = document.getElementById('stock');
+elStock.className = inStock;
+
+const elShipping = document.getElementById('shipping');
+elShipping.className = shipping;
