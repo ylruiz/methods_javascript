bbcd5b125cb0a47b6f4f09c71f4182c228ad0890
Yunet Luis Ruiz <yunet.lruiz94@gmail.com>
2019-05-08 12:44:23 -0300

    add string-with-quotes.html & string-wit-quotes.js

---
+++ c02/js/string-with-quotes.js
@@ -0,0 +1,7 @@
+const title = "Molly's spacial offers";
+const message = '<a href=\"sale.html\">25% off!</a>';
+
+const elTitle = document.getElementById('title');
+elTitle.innerHTML = title;
+const elNote = document.getElementById('note');
+elNote.innerHTML = message;
