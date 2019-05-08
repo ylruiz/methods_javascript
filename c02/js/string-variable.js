ddec93a32ab26dd25273b3c3138c4cb06ad34977
Yunet Luis Ruiz <yunet.lruiz94@gmail.com>
2019-05-08 12:01:28 -0300

    add string-variable.html and sting-variable.js

---
+++ c02/js/string-variable.js
@@ -0,0 +1,7 @@
+const username = 'Molly';
+const message = 'See our upcomming range';
+
+const elName = document.getElementById('name');
+elName.textContent = username;
+const elNote = document.getElementById('note');
+elNote.textContent = message;
