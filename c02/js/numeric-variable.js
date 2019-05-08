371e8ba773d1f29ca8dc20609a5e9a6d93512910
Yunet Luis Ruiz <yunet.lruiz94@gmail.com>
2019-05-08 12:00:55 -0300

    changed variable keyword of var el(var to const)

--- c02/js/numeric-variable.js
+++ c02/js/numeric-variable.js
@@ -2,5 +2,5 @@ const price = 5;
 const quantity = 14;
 const total = price * quantity;
 
-var el = document.getElementById('cost');
+const el = document.getElementById('cost');
 el.textContent = '$' + total;
