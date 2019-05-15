8c6cff37026e2da215f2e62fe4715908841a2cc0
Yunet Luis Ruiz <yunet.lruiz94@gmail.com>
2019-05-15 15:31:27 -0300

    add string-operator.html and string-operator.js

---
+++ c02/js/string-operator.js
@@ -0,0 +1,8 @@
+const greeting = 'Howdy ';
+const name = 'Molly';
+
+const welcomeMessage = greeting + name + '!';
+
+const el = document.getElementById('greeting');
+el.textContent = welcomeMessage;
+
